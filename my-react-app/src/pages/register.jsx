f0833cbef73888689a50d4507c65d9bb22dc5ba3
Emha2010010020 <norkhalis20@gmail.com>
2023-09-25 15:37:19 +0700

    update pelajaran 2

--- my-react-app/src/pages/register.jsx
+++ my-react-app/src/pages/register.jsx
@@ -1,11 +1,16 @@
 import AuthLayout from "../components/layout/AuthLayouts";
 import FormLogin from "../components/fragments/FormLogin";
 import FormRegister from "../components/fragments/FormRegister";
+import { Link } from "react-router-dom";
 
 const RegisterPage = () => {
     return (
     <AuthLayout title="Register">
         <FormRegister />
+        <p className="text-sm mt-5 text-center">
+            have an account?{" "}
+             <Link to="/login" className="font-bold text-blue-600">Login</Link>
+        </p>
     </AuthLayout>
  );
 };
